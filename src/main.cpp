4177052fb7b2e9f5e500f2c3e2b79290689c3419
Maschell <Maschell@gmx.de>
2022-10-06 23:12:45 +0200

    Support using the git hash on nightlies

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "main.h"
 #include "globals.h"
 #include "utils/TcpReceiver.h"
 #include "utils/logger.h"
@@ -8,7 +9,7 @@
 
 WUPS_PLUGIN_NAME("Wiiload");
 WUPS_PLUGIN_DESCRIPTION("Wiiload Server");
-WUPS_PLUGIN_VERSION("0.1");
+WUPS_PLUGIN_VERSION(VERSION_FULL);
 WUPS_PLUGIN_AUTHOR("Maschell");
 WUPS_PLUGIN_LICENSE("GPL");
 
